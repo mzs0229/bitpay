ae43a27d7f5c48057cd1fb285baf3c3ce47bb25b
mzs0229 <mzs0229@hotmail.com>
2019-12-25 17:30:33 +0000

    add stylesheet

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bitpay - @yield('title','bitpay')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">bitpay</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">help</a></li>
+            <li class="nav-item"><a href="/" class="nav-link">Login</a></li>
+        </ul>
+    </div>
+</nav>
+
+    <div class="contaiiner">
+        @yield('content')
+    </div>
+
+   
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.default')
 @section('title','Home')
 @section('content')
-    <h1>Homepage</h1>
+
+<div class="jumbotron">
+    <h1>hello bitpay</h1>
+    <p class="lead">你现在所看到的是<a href="https://www.baidu.com/">baidu</a></p>
+    <p>all start from here</p>
+    <p><a href="" class="btn btn-lg btn-success" role="button">Now Registe</a></p>
+</div>
+
+<hr color="red">
+
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
+
+
 @endsection
