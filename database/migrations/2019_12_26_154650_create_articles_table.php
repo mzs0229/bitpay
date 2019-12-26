95395335ae0817232d35c5d56a68c65e0e1a8107
mzs0229 <mzs0229@hotmail.com>
2019-12-26 15:55:22 +0000

    add database

--- database/migrations/2019_12_26_154650_create_articles_table.php
+++ database/migrations/2019_12_26_154650_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->integer('mobile')->require;
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,8 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
-
-    
 }
